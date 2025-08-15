e4f85d5ca1b7f294a13d04b05431a3752b8a4ed2
Manon Lagache <manlagache@hotmail.fr>
2025-08-15 11:03:49 +0200

    rails-watch-list progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_14_095801) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_14_103944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_14_095801) do
     t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   add_foreign_key "bookmarks", "lists"
